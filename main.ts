aa177c8dd869c54b09da0d171fd4c3646863dd1b
sun532818microbit <114642649+sun532818microbit@users.noreply.github.com>
2022-12-19 16:09:59 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Your ip is")
+    basic.showString(kittenwifi.wifi_addr())
+})
+let Your_ip = ""
+kittenwifi.wifi_init_pw(kittenwifi.SerialPorts.PORT1)
+kittenwifi.mqtt_sethost_auth(
+"Sever 1",
+"node01",
+"Player",
+"Sever1"
+)
 basic.forever(function () {
-	
+    Your_ip = kittenwifi.wifi_addr()
 })
